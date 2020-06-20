cc6d2efb7fcf413fa34e2e2edd4a8d3e240e6007
Felipe Barros <felipetech@tuta.io>
2020-06-20 11:12:08 -0300

    style: Follow standard style guide

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -6,17 +6,17 @@ class UsersController {
     const users: User[] = [
       {
         name: 'User 1',
-        email: 'user1@example.com',
+        email: 'user1@example.com'
       },
       {
         name: 'User 2',
-        email: 'user2@example.com',
+        email: 'user2@example.com'
       },
       {
         name: 'User 3',
-        email: 'user3@example.com',
-      },
-    ];
+        email: 'user3@example.com'
+      }
+    ]
 
     return res.render('index', { users })
   }
