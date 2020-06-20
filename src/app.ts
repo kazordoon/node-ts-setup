eab6464c5e3258f532530a1b15731a65292e4029
Felipe Barros <felipetech@tuta.io>
2020-06-20 10:03:20 -0300

    chore: Add express application structure

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+
+import routes from './routes'
+
+class App {
+  public express: express.Application;
+
+  constructor () {
+    this.express = express()
+
+    this.loadSettings()
+    this.loadMiddlewares()
+    this.loadRoutes()
+  }
+
+  private loadSettings () {
+    this.express.set('HOST', process.env.HOST || 'localhost')
+    this.express.set('PORT', process.env.PORT || 3333)
+  }
+
+  private loadMiddlewares () {
+    this.express.use(express.json())
+    this.express.use(express.urlencoded({ extended: false }))
+  }
+
+  private loadRoutes () {
+    this.express.use(routes)
+  }
+}
+
+export default new App().express
