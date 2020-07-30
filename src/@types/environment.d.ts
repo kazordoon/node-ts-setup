d85b2c99138b4be6978a9f244eaf9cd061739748
Felipe Barros <felipetech@tuta.io>
2020-07-30 18:27:24 -0300

    chore: Update environment.d.ts

--- src/@types/environment.d.ts
+++ src/@types/environment.d.ts
@@ -1,11 +1,7 @@
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      NODE_ENV: 'development' | 'test' | 'production'
-      HOST: string
-      PORT: number
-    }
+namespace NodeJS {
+  export interface ProcessEnv {
+    NODE_ENV: 'development' | 'test' | 'production'
+    HOST: string
+    PORT: number
   }
 }
-
-export {}
